c0e2937fd47080598919885b155fe9fed7f3fe40
B00168185 Arben Özçıkrıkcı <B00168185@mytudublin.ie>
2025-02-10 17:57:58 +0000

    added printSteps() function

---
+++ PartA.py
@@ -0,0 +1,13 @@
+#Arben Ozcikrikci
+#10/12/25
+#A Program to Print Steps 1 to 9 to the Screen
+def printSteps():
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+
+
+def main():
+    printSteps()
+
+main()
