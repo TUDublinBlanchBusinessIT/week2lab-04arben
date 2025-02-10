508a64ce2819cc35e08a5255652da4d994e8fd59
B00168185 Arben Özçıkrıkcı <B00168185@mytudublin.ie>
2025-02-10 18:17:35 +0000

    changed printSteps() function to take two arquements

---
+++ PartB.py
@@ -0,0 +1,14 @@
+#Arben Ozcikrikci
+#10/12/25
+#A Program to Print Steps 1 to 9 to the Screen
+def printSteps(a,b):
+    for i in range(a,b + 1):
+        print(f"Steps {i}")
+        
+   
+
+
+def main():
+    printSteps(1,9)
+
+main()
